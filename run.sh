39f1bd1302b3e2fe82ff4e133900c0b07166eaff
Kenneth Jung <jungeu1509@gmail.com>
2020-10-16 18:18:41 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -6,19 +6,18 @@ sudo add-apt-repository ppa:graphics-drivers/ppa
 sudo apt update
 sudo ubuntu-drivers autoinstall
 
+
 echo 'PARAM:' $0
 RELATIVE_DIR=`dirname "$0"`
-
-
 cd $RELATIVE_DIR
 SHELL_PATH=`pwd -P`
 
+
+cp vim/.vimrc ~/.vimrc
 mkdir ~/.vim
 mkdir ~/.vim/bundle
 
+
 cd ~/.vim/bundle/
 git clone https://github.com/VundleVim/Vundle.vim.git
-
-cp $SHELL_PATH/.vimrc ~/.vimrc
-
 
