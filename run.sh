b01f3762ef3ab7f631495e796e6c7fc2a3bcb63a
Kenneth Jung <jungeu1509@gmail.com>
2021-02-13 20:48:46 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,11 +5,12 @@ RELATIVE_DIR=`dirname "$0"`
 cd $RELATIVE_DIR
 SHELL_PATH=`pwd -P`
 echo "shell location : $SHELL_PATH"
+GALAXY_ROOT='/data/data/com.termux/files'
 
 echo 'export PS1="\[$(tput bold)\][\t]\[$(tput sgr0)\] \[$(tput sgr0)\]\[\033[38;5;14m\]\u@\[$(tput sgr0)\]\[\033[38;5;10m\]\H:\[$(tput sgr0)\]\[\033[38;5;203m\]\w\[$(tput sgr0)\]\[\033[38;5;15m\]\n\\$\[$(tput sgr0)\]"' >> ~/.bashrc
-echo 'export ROOT='/data/data/com.termux/files/"
+echo 'export ROOT="/data/data/com.termux/files/"'
 
-sudo apt-get install -y tree vim
+pkg install -y tree vim
 echo "Setting : download success"
 
 cp vim/.vimrc ~/.vimrc
