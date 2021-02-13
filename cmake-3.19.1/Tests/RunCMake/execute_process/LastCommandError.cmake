b7fe4a5adb9ee17bb5b63d401f13f3d39899616c
Kenneth Jung <jungeu1509@gmail.com>
2021-02-13 20:43:39 +0900

    Delete cmake-3.19.1 directory

--- cmake-3.19.1/Tests/RunCMake/execute_process/LastCommandError.cmake
+++
@@ -1,8 +0,0 @@
-execute_process(COMMAND ${CMAKE_COMMAND} -E true
-    COMMAND ${CMAKE_COMMAND} -E false
-    COMMAND ${CMAKE_COMMAND} -E false
-    COMMAND ${CMAKE_COMMAND} -E false
-    COMMAND ${CMAKE_COMMAND} -E true
-    COMMAND ${CMAKE_COMMAND} -E false
-    COMMAND_ERROR_IS_FATAL LAST
-)
