722cfd3456dab053016fa6a32f9a18198bbe1d76
Kenneth Jung <jungeu1509@gmail.com>
2021-02-14 00:59:56 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -8,7 +8,9 @@ echo "shell location : $SHELL_PATH"
 GALAXY_ROOT='/data/data/com.termux/files'
 
 echo 'export PS1="\[$(tput bold)\][\t]\[$(tput sgr0)\] \[$(tput sgr0)\]\[\033[38;5;14m\]\u@\[$(tput sgr0)\]\[\033[38;5;10m\]\H:\[$(tput sgr0)\]\[\033[38;5;203m\]\w\[$(tput sgr0)\]\[\033[38;5;15m\]\n\\$\[$(tput sgr0)\]"' >> ~/.bashrc
-echo 'export ROOT="/data/data/com.termux/files/"'
+echo 'export ROOT="/data/data/com.termux/files/"' >> ~/.bashrc
+echo 'source .bashrc_func' >> ~/.bashrc
+
 
 pkg install -y tree vim-python python nodejs yarn clang openssl-tool ripgrep
 echo "Setting : download pkg success"
@@ -24,6 +26,7 @@ cd $RELATIVE_DIR
 echo "Setting : enter ':PluginInstall' in vim"
 
 echo "yarn global add code-server"
+cp code-server/.bashrc_func $HOME/
 cp code-server/binding.gyp ~/.config/yarn/global/node_modules/code-server/lib/vscode/node_modules/spdlog/binding.gyp
 cd ~/.config/yarn/global/node_modules/code-server/lib/vscode/node_modules/spdlog/
 npm install
