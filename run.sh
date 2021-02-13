dccdae86fa0252ab0d92e11ca9878d7cca5fb7f9
Kenneth Jung <jungeu1509@gmail.com>
2021-02-13 22:03:17 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -10,7 +10,7 @@ GALAXY_ROOT='/data/data/com.termux/files'
 echo 'export PS1="\[$(tput bold)\][\t]\[$(tput sgr0)\] \[$(tput sgr0)\]\[\033[38;5;14m\]\u@\[$(tput sgr0)\]\[\033[38;5;10m\]\H:\[$(tput sgr0)\]\[\033[38;5;203m\]\w\[$(tput sgr0)\]\[\033[38;5;15m\]\n\\$\[$(tput sgr0)\]"' >> ~/.bashrc
 echo 'export ROOT="/data/data/com.termux/files/"'
 
-pkg install -y tree vim
+pkg install -y tree vim-python python nodejs yarn clang openssl-tool
 echo "Setting : download success"
 
 cp vim/.vimrc ~/.vimrc
